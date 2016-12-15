21aad64cbd147c34d71c41ed927e4c420d7086d5
swanie21 <kswanie21@gmail.com>
2016-12-14 17:30:35 -0700

    able to make api fetch request

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  TouchableHighlight,
   TextInput,
-  View
+  View,
+  AlertIOS
 } from 'react-native';
 
 export default class Search extends Component {
@@ -11,18 +13,39 @@ export default class Search extends Component {
     super();
     this.state = {
       search: '',
+      likes: '',
+      downloads: ''
     }
   }
+
+  fetchPhotographerInfo () {
+    let url = `https://api.unsplash.com/photos/random?query=${this.state.search}&client_id=f3ff11ed9e9a4de213e05ff00fa5e4f503cdf0b595de8dfd2d59cad26f7efb3f`
+    fetch(url, {method: 'GET'})
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.likes = responseData.user.total_likes
+        this.downloads = responseData.downloads
+        console.log(this.likes, this.downloads);
+      })
+    .done();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
+        <TextInput
           style={styles.input}
           autoCorrect={false}
           placeholder='Search by Location'
           onChangeText={ search => this.setState({search}) }
           value={ this.state.search }
         />
+        <TouchableHighlight
+          style={styles.submit}
+          onPress={() => {this.fetchPhotographerInfo()}}
+        >
+          <Text>Submit</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -30,16 +53,21 @@ export default class Search extends Component {
 
 const styles = StyleSheet.create({
   input: {
-    width: 100,
-    height: 20,
+    width: 200,
+    height: 30,
     textAlign: 'center',
     margin: 20,
-    backgroundColor: '#555',
+    backgroundColor: '#eee',
     padding: 5
   },
   text: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: '#111',
+    marginBottom: 5
   },
+  submit: {
+    backgroundColor: 'green',
+    height: 20,
+    width: 50
+  }
 });
